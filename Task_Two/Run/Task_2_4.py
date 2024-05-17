009163889c8d969d40bc4c6b55f074bbd80a5557
IvanVerkhovskiy <ivanverkhovskiy@yandex.ru>
2024-05-17 13:26:35 +0300

    Тема: "Ввод и вывод" (2.4)

---
+++ Task_Two/Run/Task_2_4.py
@@ -0,0 +1,41 @@
+# num1 = int(input())
+# num2 = int(input())
+# print(num1 + num2)
+# num3 = int(input())
+# num3 = str(num3)
+# print(num3)
+
+# s = 0
+# k = 30
+# d = k - 5
+# k = 2 * d
+# s = k - 100
+# print(s)
+
+# x = 3
+# y = 4
+# z = x + y
+# z = z + 1
+# x = y
+# y = 5
+# x = z + y + 7
+# print(x)
+
+# number_one = int(input())
+# number_two = number_one + 1
+# number_three = number_one + 2
+# print(number_one)
+# print(number_two)
+# print(number_three)
+
+# number_one = int(input())
+# number_two = int(input())
+# number_three = int(input())
+# number_sum = number_one + number_two + number_three
+# print(number_sum)
+
+a = int(input())
+v = a * a * a
+s = 6 * (a * a)
+print('Объем =', v)
+print('Площадь полной поверхности =', s)
---
+++ Task_Two/Run/_open_pdf.py
@@ -0,0 +1,3 @@
+import os
+path = '/Users/ivan.verkhovskiy/Desktop/Документы_Ali/Онбординг.pdf'
+os.system(path)
