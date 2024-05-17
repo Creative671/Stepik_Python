58874fdf0b0f50297bbfb92f250481331bf6b6fb
IvanVerkhovskiy <ivanverkhovskiy@yandex.ru>
2024-05-17 13:52:32 +0300

    Тема: "Ввод и вывод" (2.4)

--- Task_Two/Run/Task_2_4.py
+++ Task_Two/Run/Task_2_4.py
@@ -34,8 +34,39 @@
 # number_sum = number_one + number_two + number_three
 # print(number_sum)
 
+# a = int(input())
+# v = a * a * a
+# s = 6 * (a * a)
+# print('Объем =', v)
+# print('Площадь полной поверхности =', s)
+
+# a = int(input())
+# b = int(input())
+# print(3 * ((a + b) * (a + b) * (a + b)) + 275 * b * b - 127 * a - 41)
+
+# a = int(input())
+# nextNumber = a + 1
+# prevNumber = a - 1
+# print('Следующее за числом', a, 'число:', nextNumber)
+# print('Для числа', a, 'предыдущее число:', prevNumber)
+
+# numberOne = int(input())
+# numberTwo = int(input())
+# numberThree = int(input())
+# numberFour = int(input())
+# print((numberOne + numberTwo + numberThree + numberFour) * 3)
+
+# a = int(input())
+# b = int(input())
+# print(a, '+', b, '=', a + b)
+# print(a, '-', b, '=', a - b)
+# print(a, '*', b, '=', a * b)
+
+# a = int(input())
+# d = int(input())
+# n = int(input())
+#
+# print(a + d * (n - 1))
+
 a = int(input())
-v = a * a * a
-s = 6 * (a * a)
-print('Объем =', v)
-print('Площадь полной поверхности =', s)
+print(a, a * 2, a * 3, a * 4, a * 5, sep='---')
